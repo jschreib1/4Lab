a782e8f46b84a78ec7a400e6bcc143d7e5414765
jschreib1 <86371980+jschreib1@users.noreply.github.com>
2021-10-20 17:44:32 +0000

    Initial commit

---
+++ pwm.py
@@ -0,0 +1,26 @@
+#!/usr/bin/cgipython1
+
+import cgi
+import json
+import cgitb
+cgitb.enable()
+
+
+print("Content-type: text/html\n\n")
+data= cgi.FieldStorage()
+s1 = data.getvalue('slider1')
+s2 = data.getvalue('LED')
+data = {"slider1":s1, "LED":s2}
+with open('led-pwm-multiple.txt', 'w') as f:
+  json.dump(data,f)
+
+print('<html>')
+print('<form action="/cgi-bin/pwm.py" method="POST">')
+print('<input type="radio" name="LED" value="a" checked> LED A <br>')
+print('<input type="radio" name="LED" value="b"> LED B <br>')
+print('<input type="radio" name="LED" value="c"> LED C <br>')
+print('<input type="range" name="slider1" min="0" max="100" value="%s"><br>' % s1)
+print('<input type="submit" value="Change LED brightness">')
+print('</form>')
+print('Brightness = %s' % s1)
+print('</html>')
---
+++ pwmBackground.py
@@ -0,0 +1,36 @@
+import json
+import cgi
+import RPi.GPIO as GPIO #import RPi.GPIO module
+from time import sleep # import time.sleep()
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)
+pins = {}
+p1 = 4
+p2 = 17
+p3 = 13
+GPIO.setup(p1, GPIO.OUT) 
+GPIO.setup(p2, GPIO.OUT) 
+GPIO.setup(p3, GPIO.OUT)
+fr = 1000 # frequency (Hz)
+#dc = 50 # duty cycle (%)
+pwm1 = GPIO.PWM(p1,fr)
+pwm2 = GPIO.PWM(p2,fr)
+pwm3 = GPIO.PWM(p3,fr)
+
+pwm1.start(0) # start with LED off
+pwm2.start(0)
+pwm3.start(0)
+while True:
+  with open("led-pwm-multiple.txt", 'r') as f:
+    data = json.load(f) # read duty cycle value from file
+    dc = int(data['slider1'])
+    led = int(data['LED'])
+  if led == 1: 
+    pwm1.ChangeDutyCycle(dc)
+  elif led == 2:
+    pwm2.ChangeDutyCycle(dc)
+  elif led == 3:
+    pwm3.ChangeDutyCycle(dc)
+  
+  sleep(0.1)
